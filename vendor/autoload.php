77f9d67da7a28587db57cf5fb391480533742398
Raihan Ahmed Shuvo <rshuvo1621@gmail.com>
2026-03-03 22:14:52 +0600

    Add files via upload

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,1 +1,9 @@
+<?php
+/**
+ * Composer autoload replacement for manual PHPMailer installation
+ */
 
+// PHPMailer classes
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require_once __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';
